630a876f16d88806de498019d352b061d8c80d7c
brunomartins-rdev <bruno.silva.martins@hotmail.com>
2025-06-17 12:38:43 +0100

    Autocompletion

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,26 +1,29 @@
--- lua/plugins/lspconfig.lua
 return {
   "neovim/nvim-lspconfig",
+  dependencies = { "hrsh7th/cmp-nvim-lsp" }, -- make sure this is installed!
   config = function()
     local lspconfig = require("lspconfig")
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    -- Python
-    lspconfig.pyright.setup({})
-
-    -- R
-    lspconfig.r_language_server.setup({})
-
-    -- Go
-    lspconfig.gopls.setup({
-      settings = {
-        gopls = {
-          analyses = {
-            unusedparams = true,
+    local servers = {
+      pyright = {},
+      r_language_server = {},
+      gopls = {
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
           },
-          staticcheck = true,
         },
       },
-    })
+    }
+
+    for server, opts in pairs(servers) do
+      opts.capabilities = capabilities
+      lspconfig[server].setup(opts)
+    end
   end,
 }
 
